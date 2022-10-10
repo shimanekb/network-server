6a30f2c5b5d9eb5f8f66a6d5cc2c46c9eb753ee6
shimanekb <brandon.j.shimanek@gmail.com>
2022-10-10 07:50:17 -0500

    http request neg cases checked

--- tests/test_http.py
+++ tests/test_http.py
@@ -1,13 +1,14 @@
 from textwrap import dedent
+
+import pytest
+
 import server.http as http
-
-
 
 
 def test_http_request_parse_url_from_valid_request():
     # Given
     raw_request = """\
-                  GET /foo/stuff.html HTTP/1.0\r
+                  GET /foo/stuff.html\r
                   Host: www.localhost:9090\r
                   Connection: close\r
                   User-agent: Chrome\r
@@ -19,3 +20,27 @@ def test_http_request_parse_url_from_valid_request():
 
     # Then
     assert http_req.url == '/foo/stuff.html'
+
+
+def test_http_request_empty_request():
+    with pytest.raises(ValueError):
+        # Given
+        raw_request = ''
+
+        # When
+        http.HttpRequest(raw_request)
+
+
+def test_http_request_invalid_request_line():
+    with pytest.raises(ValueError):
+        # Given
+        raw_request = """\
+                      GET /foo/stuff.html\r
+                      Host: www.localhost:9090\r
+                      Connection: close\r
+                      User-agent: Chrome\r
+                      Accept-language: en\r"""
+        raw_request = dedent(raw_request)
+
+        # When
+        http.HttpRequest(raw_request)
